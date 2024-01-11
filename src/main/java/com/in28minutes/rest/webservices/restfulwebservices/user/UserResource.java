53f242e5dfa982a7b335faf9f8640221fbb43eb4
CruzInc <abstractime@outlook.com>
2024-01-11 15:25:44 -0600

    When  User is null while geting users, UserNotFoundException is implemented. This will be shown on 'White error page'

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -25,12 +28,27 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id){
-		return service.findOne(id);
+		User user = service.findOne(id);
+		
+		if (user == null) {			
+			// There was already an exception that was thrown, this just customize that exception
+			// The result could be observed in the White label page. 
+			throw new UserNotFoundException("id:" + id);
+		}
+		
+		return user;
 	}
 	
 	// POST /users
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 		service.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")				
+				.buildAndExpand(user.getId())
+				.toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
