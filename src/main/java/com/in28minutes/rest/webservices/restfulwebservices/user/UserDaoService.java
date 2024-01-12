9fe5f4a8ebb0c84035c7f76c4566c93c670fd874
CruzInc <abstractime@outlook.com>
2024-01-11 18:05:53 -0600

    Delete user using DeleteMapping and users.remove(predicate)

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -35,4 +35,10 @@ public class UserDaoService {
 		
 		return user;
 	}
+
+    	public void deleteById(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+
+		users.removeIf(predicate);
+	}
 }
