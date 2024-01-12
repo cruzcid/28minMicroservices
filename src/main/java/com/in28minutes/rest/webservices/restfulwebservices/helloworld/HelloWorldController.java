af4aa0fc1d510ba1f18eff0f2599800d50791aa7
CruzInc <abstractime@outlook.com>
2024-01-11 23:46:31 -0600

    Added i18n for internationalization. File messages.properties added at application.properties file. To add i18n files with target language should be created eg: messages_fr.properties for french, messages_nl.properties for Dutch and so on. The client should request the specific language as a header eg: Accept-Language : nl

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 // REST API	
 @RestController
 public class HelloWorldController { 
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	public String helloWorld() {
@@ -24,5 +34,16 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		// 1: en: English
+		// 2: fr: French
+		// For i18n to work two things are needed. 
+		// 	  There must be a file at the same level of application.properties
+		//    The file must be named messages.properties
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default message", locale);		
 	}
 }
