1f99b56366ab7c36d09f83e19e1751c73ea05e25
CruzInc <abstractime@outlook.com>
2024-01-12 01:55:03 -0600

    Versioning styles added. URI Twitter, Params Amazon, Header Microsoft, Media Github

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -5,13 +5,48 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+	// URI Versioning AKA Twitter versioning
 	@GetMapping("/v1/person")
 	public PersonV1 getFirstVersionOfPerson() {
 		return new PersonV1("Bob Charlie");
 	}
-	
+
 	@GetMapping("/v2/person")
 	public PersonV2 getSecondVersionOfPerson() {
 		return new PersonV2(new Name("Bob", "Esponja"));
 	}
+
+	// Params versioning AKA Amazon versioning
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParameter() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getSecondVersionOfPersonRequestParameter() {
+		return new PersonV2(new Name("Bob", "Esponja"));
+	}
+
+	// Headers versioning AKA Microsoft versioning
+	@GetMapping(path = "/person/headers", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeaders() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(path = "/person/headers", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeaders() {
+		return new PersonV2(new Name("Bob", "Esponja"));
+	}
+
+	// Media type versioning AKA 'content negotiation' or 'Accept Header' Github
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+		return new PersonV2(new Name("Bob", "Esponja"));
+	}
+
 }
