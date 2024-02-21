8c357ae20beb262bc16244632870333851bae8ca
CruzInc <abstractime@outlook.com>
2024-02-21 15:49:07 -0600

    Post Entity created, ManyToOne with lazy fetchtype. Added List of Post to User entity with OneToMany(mappedBy = "user"). Insert Posts in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,7 @@ insert into user_details(id, birth_date, name) values (10001, current_date(), 'c
 insert into user_details(id, birth_date, name) values (10002, current_date(), 'ravi');
 
 insert into user_details(id, birth_date, name) values (10003, current_date(), 'shankar');
+
+insert into post(id, description, user_id) values (20001, 'I want to fly', 10001);
+
+insert into post(id, description, user_id) values (20002, 'What is the fastest way to learn?', 10001);
